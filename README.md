d1c41a722b03f7bd73c54f03e6e2e6cf929fb67f
Gabriel Horner <gabriel.horner@gmail.com>
2014-02-07 15:37:42 -0500

    preppin for a first cut

---
+++ README.md
@@ -0,0 +1,22 @@
+## Description
+This project improves on Light Table's default plugins manager. It provides one command
+to save plugins to a file and another to install plugins from that file.
+
+## Usage
+
+Save your plugins with the command `plugins-inc: Save plugins...`.
+
+Your plugins will be saved to `plugins.edn` in your settings folder. On a mac,
+this file will be at `/Users/$USER/Library/Application Support/LightTable/settings/plugins.edn`
+
+After editing your plugins.edn or if using LT from a new machine, install
+all your plugins with the command `plugins-inc: Update plugins...`.
+
+## Bugs/Issues
+Please report them [on github](http://github.com/cldwalker/lt-plugins-inc/issues).
+
+## TODO
+* Install a plugin given a git url and sha.
+
+## License
+See LICENSE.TXT
--- plugins_inc_compiled.js
+++ plugins_inc_compiled.js
@@ -20,7 +20,7 @@ goog.require('lt.objs.console');
 goog.require('lt.objs.command');
 goog.require('lt.objs.deploy');
 lt.plugins.plugins_inc.plugins_file = lt.objs.files.join.call(null,lt.objs.files.lt_user_dir.call(null,"settings"),"plugins.edn");
-lt.plugins.plugins_inc.validate_plugins = (function validate_plugins(plugins){if(cljs.core.every_QMARK_.call(null,(function (p1__9754_SHARP_){return cljs.core.every_QMARK_.call(null,p1__9754_SHARP_,new cljs.core.PersistentHashSet(null, new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"name","name",1017277949),null,new cljs.core.Keyword(null,"version","version",1365512266),null,new cljs.core.Keyword(null,"source","source",4412365709),null], null), null));
+lt.plugins.plugins_inc.validate_plugins = (function validate_plugins(plugins){if(cljs.core.every_QMARK_.call(null,(function (p1__8189_SHARP_){return cljs.core.every_QMARK_.call(null,p1__8189_SHARP_,new cljs.core.PersistentHashSet(null, new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"name","name",1017277949),null,new cljs.core.Keyword(null,"version","version",1365512266),null,new cljs.core.Keyword(null,"source","source",4412365709),null], null), null));
 }),plugins))
 {return plugins;
 } else
@@ -30,8 +30,8 @@ lt.plugins.plugins_inc.validate_plugins = (function validate_plugins(plugins){if
 lt.plugins.plugins_inc.read_plugins = (function read_plugins(file){return lt.plugins.plugins_inc.validate_plugins.call(null,lt.objs.settings.safe_read.call(null,new cljs.core.Keyword(null,"content","content",1965434859).cljs$core$IFn$_invoke$arity$1(lt.objs.files.open_sync.call(null,file)),file));
 });
 lt.plugins.plugins_inc.update_plugins = (function update_plugins(){var existing_plugins = new cljs.core.Keyword("lt.objs.plugins","plugins","lt.objs.plugins/plugins",2920328683).cljs$core$IFn$_invoke$arity$1(cljs.core.deref.call(null,lt.objs.app.app));var outdated_plugins = cljs.core.filter.call(null,((function (existing_plugins){
-return (function (p1__9755_SHARP_){var temp__4090__auto__ = cljs.core.get_in.call(null,existing_plugins,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"name","name",1017277949).cljs$core$IFn$_invoke$arity$1(p1__9755_SHARP_),new cljs.core.Keyword(null,"version","version",1365512266)], null));if(cljs.core.truth_(temp__4090__auto__))
-{var old_version = temp__4090__auto__;return lt.objs.deploy.is_newer_QMARK_.call(null,old_version,new cljs.core.Keyword(null,"version","version",1365512266).cljs$core$IFn$_invoke$arity$1(p1__9755_SHARP_));
+return (function (p1__8190_SHARP_){var temp__4090__auto__ = cljs.core.get_in.call(null,existing_plugins,new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"name","name",1017277949).cljs$core$IFn$_invoke$arity$1(p1__8190_SHARP_),new cljs.core.Keyword(null,"version","version",1365512266)], null));if(cljs.core.truth_(temp__4090__auto__))
+{var old_version = temp__4090__auto__;return lt.objs.deploy.is_newer_QMARK_.call(null,old_version,new cljs.core.Keyword(null,"version","version",1365512266).cljs$core$IFn$_invoke$arity$1(p1__8190_SHARP_));
 } else
 {return true;
 }
@@ -39,45 +39,45 @@ return (function (p1__9755_SHARP_){var temp__4090__auto__ = cljs.core.get_in.cal
 ,lt.plugins.plugins_inc.read_plugins.call(null,lt.plugins.plugins_inc.plugins_file));if(cljs.core.empty_QMARK_.call(null,outdated_plugins))
 {return lt.objs.notifos.set_msg_BANG_.call(null,"No plugins to update.");
 } else
-{var seq__9760_9765 = cljs.core.seq.call(null,outdated_plugins);var chunk__9761_9766 = null;var count__9762_9767 = 0;var i__9763_9768 = 0;while(true){
-if((i__9763_9768 < count__9762_9767))
-{var p_9769 = cljs.core._nth.call(null,chunk__9761_9766,i__9763_9768);lt.objs.plugins.discover_deps.call(null,p_9769);
+{var seq__8195_8210 = cljs.core.seq.call(null,outdated_plugins);var chunk__8196_8211 = null;var count__8197_8212 = 0;var i__8198_8213 = 0;while(true){
+if((i__8198_8213 < count__8197_8212))
+{var p_8214 = cljs.core._nth.call(null,chunk__8196_8211,i__8198_8213);lt.objs.plugins.discover_deps.call(null,p_8214);
 {
-var G__9770 = seq__9760_9765;
-var G__9771 = chunk__9761_9766;
-var G__9772 = count__9762_9767;
-var G__9773 = (i__9763_9768 + 1);
-seq__9760_9765 = G__9770;
-chunk__9761_9766 = G__9771;
-count__9762_9767 = G__9772;
-i__9763_9768 = G__9773;
+var G__8215 = seq__8195_8210;
+var G__8216 = chunk__8196_8211;
+var G__8217 = count__8197_8212;
+var G__8218 = (i__8198_8213 + 1);
+seq__8195_8210 = G__8215;
+chunk__8196_8211 = G__8216;
+count__8197_8212 = G__8217;
+i__8198_8213 = G__8218;
 continue;
 }
 } else
-{var temp__4092__auto___9774 = cljs.core.seq.call(null,seq__9760_9765);if(temp__4092__auto___9774)
-{var seq__9760_9775__$1 = temp__4092__auto___9774;if(cljs.core.chunked_seq_QMARK_.call(null,seq__9760_9775__$1))
-{var c__7485__auto___9776 = cljs.core.chunk_first.call(null,seq__9760_9775__$1);{
-var G__9777 = cljs.core.chunk_rest.call(null,seq__9760_9775__$1);
-var G__9778 = c__7485__auto___9776;
-var G__9779 = cljs.core.count.call(null,c__7485__auto___9776);
-var G__9780 = 0;
-seq__9760_9765 = G__9777;
-chunk__9761_9766 = G__9778;
-count__9762_9767 = G__9779;
-i__9763_9768 = G__9780;
+{var temp__4092__auto___8219 = cljs.core.seq.call(null,seq__8195_8210);if(temp__4092__auto___8219)
+{var seq__8195_8220__$1 = temp__4092__auto___8219;if(cljs.core.chunked_seq_QMARK_.call(null,seq__8195_8220__$1))
+{var c__7485__auto___8221 = cljs.core.chunk_first.call(null,seq__8195_8220__$1);{
+var G__8222 = cljs.core.chunk_rest.call(null,seq__8195_8220__$1);
+var G__8223 = c__7485__auto___8221;
+var G__8224 = cljs.core.count.call(null,c__7485__auto___8221);
+var G__8225 = 0;
+seq__8195_8210 = G__8222;
+chunk__8196_8211 = G__8223;
+count__8197_8212 = G__8224;
+i__8198_8213 = G__8225;
 continue;
 }
 } else
-{var p_9781 = cljs.core.first.call(null,seq__9760_9775__$1);lt.objs.plugins.discover_deps.call(null,p_9781);
+{var p_8226 = cljs.core.first.call(null,seq__8195_8220__$1);lt.objs.plugins.discover_deps.call(null,p_8226);
 {
-var G__9782 = cljs.core.next.call(null,seq__9760_9775__$1);
-var G__9783 = null;
-var G__9784 = 0;
-var G__9785 = 0;
-seq__9760_9765 = G__9782;
-chunk__9761_9766 = G__9783;
-count__9762_9767 = G__9784;
-i__9763_9768 = G__9785;
+var G__8227 = cljs.core.next.call(null,seq__8195_8220__$1);
+var G__8228 = null;
+var G__8229 = 0;
+var G__8230 = 0;
+seq__8195_8210 = G__8227;
+chunk__8196_8211 = G__8228;
+count__8197_8212 = G__8229;
+i__8198_8213 = G__8230;
 continue;
 }
 }
@@ -90,8 +90,8 @@ return lt.objs.notifos.set_msg_BANG_.call(null,"Plugins have been updated.");
 }
 });
 lt.objs.command.command.call(null,new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"command","command",1964298941),new cljs.core.Keyword("lt.plugins.plugins-inc","update-plugins","lt.plugins.plugins-inc/update-plugins",3537532994),new cljs.core.Keyword(null,"desc","desc",1016984067),"plugins-inc: Update plugins from plugins.edn",new cljs.core.Keyword(null,"exec","exec",1017031683),lt.plugins.plugins_inc.update_plugins], null));
-lt.plugins.plugins_inc.save_plugins = (function save_plugins(){var plugins_str_9786 = cljs.core.pr_str.call(null,cljs.core.vec.call(null,cljs.core.map.call(null,(function (p1__9764_SHARP_){return cljs.core.select_keys.call(null,p1__9764_SHARP_,new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"name","name",1017277949),new cljs.core.Keyword(null,"version","version",1365512266),new cljs.core.Keyword(null,"source","source",4412365709)], null));
-}),cljs.core.vals.call(null,new cljs.core.Keyword("lt.objs.plugins","plugins","lt.objs.plugins/plugins",2920328683).cljs$core$IFn$_invoke$arity$1(cljs.core.deref.call(null,lt.objs.app.app))))));lt.objs.files.save.call(null,lt.plugins.plugins_inc.plugins_file,clojure.string.replace.call(null,plugins_str_9786,/\}\s*\{/,"}\n {"));
+lt.plugins.plugins_inc.save_plugins = (function save_plugins(){var plugins_str_8231 = cljs.core.pr_str.call(null,cljs.core.vec.call(null,cljs.core.map.call(null,(function (p1__8199_SHARP_){return cljs.core.select_keys.call(null,p1__8199_SHARP_,new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"name","name",1017277949),new cljs.core.Keyword(null,"version","version",1365512266),new cljs.core.Keyword(null,"source","source",4412365709)], null));
+}),cljs.core.vals.call(null,new cljs.core.Keyword("lt.objs.plugins","plugins","lt.objs.plugins/plugins",2920328683).cljs$core$IFn$_invoke$arity$1(cljs.core.deref.call(null,lt.objs.app.app))))));lt.objs.files.save.call(null,lt.plugins.plugins_inc.plugins_file,clojure.string.replace.call(null,plugins_str_8231,/\}\s*\{/,"}\n {"));
 return lt.objs.notifos.set_msg_BANG_.call(null,"Plugins saved to plugins.edn.");
 });
 lt.objs.command.command.call(null,new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"command","command",1964298941),new cljs.core.Keyword("lt.plugins.plugins-inc","save-plugins","lt.plugins.plugins-inc/save-plugins",4742107022),new cljs.core.Keyword(null,"desc","desc",1016984067),"plugins-inc: Save plugins to plugins.edn",new cljs.core.Keyword(null,"exec","exec",1017031683),lt.plugins.plugins_inc.save_plugins], null));
